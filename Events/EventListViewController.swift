30f8e1396700741786a43f22c6588509c0935f24
Alexander <a.pride.xxi@tutanota.com>
2023-06-28 11:14:31 +0300

    feat: Created method views for EventListViewController

--- Events/EventListViewController.swift
+++ Events/EventListViewController.swift
@@ -9,12 +9,38 @@ import UIKit
 
 class EventListViewController: UIViewController {
 
+    private enum Constants {
+        static var plusButtonTitle = "plus.circle.fill"
+        static var title = "Events"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .gray
+        views()
+    }
+    
+    private func views() {
+        view.backgroundColor = .systemBackground
+        
+        // navigationBar
+        let pluseImage = UIImage(systemName: Constants.plusButtonTitle)
+        let barButtonItem = UIBarButtonItem(
+            image: pluseImage,
+            style: .plain,
+            target: self,
+            action: #selector(tappedRightBarButton)
+        )
+        barButtonItem.tintColor = .primary
+        navigationItem.rightBarButtonItem = barButtonItem
+        navigationItem.title = Constants.title
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 
+    @objc private func tappedRightBarButton() {
+        print("tap-tap-tap")
+    }
 
 }
 
