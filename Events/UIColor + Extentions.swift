1e0db66181c4d4dff7496d7c2513a46acabef0e0
Alexander <a.pride.xxi@tutanota.com>
2023-06-28 10:50:28 +0300

    feat: UIColor extention primary

---
+++ Events/UIColor + Extentions.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor + Extentions.swift
+//  Events
+//
+//  Created by Александр Прайд on 28.06.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(red: 0/255, green: 39/255, blue: 255/255, alpha: 0.6)
+}
